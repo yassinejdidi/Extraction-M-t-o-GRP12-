db56fc14afd375f2c9ba97c83e2f767edd31a7da
aureane <aureane.walla@gmail.com>
2025-11-05 15:27:36 +0100

    Mise à jour du script Extracteur_Météo.sh et filemeteo.txt

--- Extracteur_Météo.sh
+++ Extracteur_Météo.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+$(grep "°C" filemeteo.txt | head -n 1 | awk '{print $2}')#!/bin/bash
 
 read -p "Entrez le nom de la ville : " ville
 echo " Météo pour $ville " >> filemeteo.txt
-curl wttr.in/$ville_url >> filemeteo.txt
-#echo -e "\n\n" >> $fichier  ajoute une ligne vide entre deux villes
+curl wttr.in/$ville >> filemeteo.txt
 
-echo "Les données météo pour $ville ont été ajoutées."
+echo "Température actuelle de la ville : $(grep "°C" filemeteo.txt | head -n 1 )"
+
+echo "Prévision meteo pour demain : $(grep "°C" filemeteo.txt | head -n 2 | tail -n 1)"
+echo "Les données météo pour $ville sont disponible vous pouvez les consulter."
