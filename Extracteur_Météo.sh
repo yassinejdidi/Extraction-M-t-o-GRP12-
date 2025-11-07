3c6b5b226bdf69246b8757852fc79f172c29ccab
yassinejdidi <yassinejdidi199955@gmail.com>
2025-11-07 15:23:06 +0100

    Script avec ville par défaut et quelques modifications

--- Extracteur_Météo.sh
+++ Extracteur_Météo.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 
 FICHIER_SORTIE="meteo.txt"
+FICHIER_LOCAL="local.txt"
 WTTR_URL="wttr.in"
+VILLE_DEFAUT="Toulouse"
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 <Ville>"
-    exit 1
+    VILLE="$VILLE_DEFAUT"
+    echo "Vous n'avez pas mis une ville en argument , soit la ville par défaut : $VILLE"
+else
+    VILLE="$1"
 fi
 
-VILLE="$1"
-DATE=$(date +"%d/%m/%Y")
-HEURE=$(date +"%H:%M:%S")
+DATE=$(date +"%Y-%m-%d")
+HEURE=$(date +"%H:%M")
+
+curl -s "$WTTR_URL/$VILLE?format=j1" > "$FICHIER_LOCAL"
 
 if ! command -v jq &> /dev/null; then
     echo "Erreur : jq n'est pas installé."
     exit 1
 fi
 
-METEO_JSON=$(curl -s "$WTTR_URL/$VILLE?format=j1")
-
-TEMP_ACTUELLE=$(echo "$METEO_JSON" | jq -r '.current_condition[0].temp_C')
+TEMP_ACTUELLE=$(jq -r '.current_condition[0].temp_C' "$FICHIER_LOCAL")
 [ -z "$TEMP_ACTUELLE" ] && TEMP_ACTUELLE="Non disponible"
 TEMP_ACTUELLE="${TEMP_ACTUELLE}°C"
 
-TEMP_DEMAIN=$(echo "$METEO_JSON" | jq -r '.weather[1].avgtempC')
+TEMP_DEMAIN=$(jq -r '.weather[1].avgtempC' "$FICHIER_LOCAL")
 [ -z "$TEMP_DEMAIN" ] && TEMP_DEMAIN="Non disponible"
 TEMP_DEMAIN="${TEMP_DEMAIN}°C"
 
 echo "Ville : $VILLE"
-echo "Température actuelle : $TEMP_ACTUELLE"
-echo "Température prévue pour demain : $TEMP_DEMAIN"
+echo "Temperature actuelle : $TEMP_ACTUELLE"
+echo "Temperature prevue pour demain : $TEMP_DEMAIN"
 
-echo "$DATE - $HEURE - Ville : $VILLE - $TEMP_ACTUELLE - $TEMP_DEMAIN" >> "$FICHIER_SORTIE"
+echo "$DATE -$HEURE -$VILLE : $TEMP_ACTUELLE - $TEMP_DEMAIN" >> "$FICHIER_SORTIE"
 
 echo "Données enregistrées dans $FICHIER_SORTIE"
 exit 0
-
