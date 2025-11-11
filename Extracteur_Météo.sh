9ecc21026ed4914b4ea07c3184f7b8be6a80c78d
qutidu <alfrcoug99@gmail.com>
2025-11-11 18:25:42 +0100

    je viens d'ajouter l'historique dans un fichier( format meteo_YYYYMMDD.txt)

--- Extracteur_Météo.sh
+++ Extracteur_Météo.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-FICHIER_SORTIE="meteo.txt"
 FICHIER_LOCAL="local.txt"
 WTTR_URL="wttr.in"
 VILLE_DEFAUT="Toulouse"
 
 if [ -z "$1" ]; then
     VILLE="$VILLE_DEFAUT"
-    echo "Vous n'avez pas mis une ville en argument , soit la ville par défaut : $VILLE"
+    echo "Vous n'avez pas mis de ville en argument, utilisation de la ville par défaut : $VILLE"
 else
     VILLE="$1"
 fi
 
 DATE=$(date +"%Y-%m-%d")
 HEURE=$(date +"%H:%M")
+
+fichier_sortie="meteo_$(date +"%Y%m%d").txt"
 
 curl -s "$WTTR_URL/$VILLE?format=j1" > "$FICHIER_LOCAL"
 
@@ -31,10 +32,10 @@ TEMP_DEMAIN=$(jq -r '.weather[1].avgtempC' "$FICHIER_LOCAL")
 TEMP_DEMAIN="${TEMP_DEMAIN}°C"
 
 echo "Ville : $VILLE"
-echo "Temperature actuelle : $TEMP_ACTUELLE"
-echo "Temperature prevue pour demain : $TEMP_DEMAIN"
+echo "Température actuelle : $TEMP_ACTUELLE"
+echo "Température prévue pour demain : $TEMP_DEMAIN"
 
-echo "$DATE -$HEURE -$VILLE : $TEMP_ACTUELLE - $TEMP_DEMAIN" >> "$FICHIER_SORTIE"
+echo "$DATE $HEURE - $VILLE : $TEMP_ACTUELLE / $TEMP_DEMAIN" >> "$fichier_sortie"
 
-echo "Données enregistrées dans $FICHIER_SORTIE"
+echo "Données enregistrées dans $fichier_sortie"
 exit 0
