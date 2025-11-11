4e55ae82c72d6a8a7eeb19f9198881abb667f339
qutidu <alfrcoug99@gmail.com>
2025-11-11 18:04:03 +0100

    Récupération du script depuis la branche de yassine

--- Extracteur_Météo.sh
+++ Extracteur_Météo.sh
@@ -1,45 +1,40 @@
 #!/bin/bash
 
-FICHIER_METEO="filemeteo.txt"
+FICHIER_SORTIE="meteo.txt"
+FICHIER_LOCAL="local.txt"
 WTTR_URL="wttr.in"
-LANGUE="fr"
+VILLE_DEFAUT="Toulouse"
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 <Ville>"
-    echo "Exemple: ./Extracteur_Météo.sh Toulouse"
+    VILLE="$VILLE_DEFAUT"
+    echo "Vous n'avez pas mis une ville en argument , soit la ville par défaut : $VILLE"
+else
+    VILLE="$1"
+fi
+
+DATE=$(date +"%Y-%m-%d")
+HEURE=$(date +"%H:%M")
+
+curl -s "$WTTR_URL/$VILLE?format=j1" > "$FICHIER_LOCAL"
+
+if ! command -v jq &> /dev/null; then
+    echo "Erreur : jq n'est pas installé."
     exit 1
 fi
 
-VILLE="$1"
+TEMP_ACTUELLE=$(jq -r '.current_condition[0].temp_C' "$FICHIER_LOCAL")
+[ -z "$TEMP_ACTUELLE" ] && TEMP_ACTUELLE="Non disponible"
+TEMP_ACTUELLE="${TEMP_ACTUELLE}°C"
 
-echo "Recuperation des donnees meteorologiques pour $VILLE..."
-
-curl -s "$LANGUE.$WTTR_URL/$VILLE?format=3" > "$FICHIER_METEO"
-
-if [ $? -ne 0 ] || [ ! -s "$FICHIER_METEO" ]; then
-    echo "Erreur: Impossible de recuperer les donnees ou fichier vide."
-    rm -f "$FICHIER_METEO"
-    exit 1
-fi
-
-echo "Donnees sauvegardees dans $FICHIER_METEO."
-
-TEMP_ACTUELLE_LIGNE=$(head -n 3 "$FICHIER_METEO" | tail -n 1)
-TEMP_ACTUELLE=$(echo "$TEMP_ACTUELLE_LIGNE" | grep -oE '[+-]?[0-9]+' | head -n 1)
-
-if [ -z "$TEMP_ACTUELLE" ]; then
-    TEMP_ACTUELLE="Non disponible"
-fi
-
-TEMP_DEMAIN_LIGNE=$(head -n 5 "$FICHIER_METEO" | tail -n 1)
-TEMP_DEMAIN=$(echo "$TEMP_DEMAIN_LIGNE" | awk '{print $NF}' | sed 's/+//; s/±//')
-
-if [ -z "$TEMP_DEMAIN" ]; then
-    TEMP_DEMAIN="Non disponible"
-fi
+TEMP_DEMAIN=$(jq -r '.weather[1].avgtempC' "$FICHIER_LOCAL")
+[ -z "$TEMP_DEMAIN" ] && TEMP_DEMAIN="Non disponible"
+TEMP_DEMAIN="${TEMP_DEMAIN}°C"
 
 echo "Ville : $VILLE"
-echo "Temperature actuelle : $TEMP_ACTUELLE°C"
+echo "Temperature actuelle : $TEMP_ACTUELLE"
 echo "Temperature prevue pour demain : $TEMP_DEMAIN"
 
+echo "$DATE -$HEURE -$VILLE : $TEMP_ACTUELLE - $TEMP_DEMAIN" >> "$FICHIER_SORTIE"
+
+echo "Données enregistrées dans $FICHIER_SORTIE"
 exit 0
